cdcc53cc45932f0549ff2ea9266d2c1f32520545
tbengric <168076434+tbengric@users.noreply.github.com>
2024-04-30 19:00:41 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ def main():
     b.pplot()
     for move in b.legals(1):
         print(move[0], move[1])
-
+    r = play(b, 1, (Hex(-3, 0), Hex(-2, 1)))
+    r.pplot()
 
 if __name__ == "__main__":
     main()
