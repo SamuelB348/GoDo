d8b82ab5f29937d88de59339bab34ee1cdc80716
tbengric <168076434+tbengric@users.noreply.github.com>
2024-04-30 19:33:10 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,12 +2,7 @@ from utils import *
 
 
 def main():
-    b = start_board(4)
-    b.pplot()
-    for move in b.legals(1):
-        print(move[0], move[1])
-    r = play(b, 1, (Hex(-3, 0), Hex(-2, 1)))
-    r.pplot()
+    return dodo(strategy_brain, strategy_brain)
 
 if __name__ == "__main__":
     main()
