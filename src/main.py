94a6f2ed97beafbf78edfdffd70bfcaaab46ebec
Samuel.B <samuelbeziat64@gmail.com>
2024-05-01 21:06:02 +0200

    qq changements de classe et de méthode, boucle de jeu ok

--- src/main.py
+++ src/main.py
@@ -2,28 +2,13 @@ from utils import *
 import time
 
 
-def test_play1():
-    b = start_board(4)
-    total_time = 0
-
-    for i in range(5000):
-        start_time = time.time()
-        play(b, 1, (Hex(-3, 0), Hex(-2, 1)))
-        end_time = time.time()
-        execution_time = end_time - start_time
-        total_time += execution_time
-
-    average_time = total_time / 5000
-    return average_time
-
-
 def test_play2():
     b = start_board(4)
     total_time = 0
 
     for i in range(5000):
         start_time = time.time()
-        play2(b, 1, (Hex(-3, 0), Hex(-2, 1)))
+        b.play(1, (Hex(-3, 0), Hex(-2, 1)))
         end_time = time.time()
         execution_time = end_time - start_time
         total_time += execution_time
@@ -33,15 +18,12 @@ def test_play2():
 
 
 def test_dodo(n):
-    return dodo(strategy_brain, strategy_brain, n)
+    return dodo(strategy_random, strategy_random, n)
 
 
 def main():
-    # test_dodo(4)
-    a = test_play1()
-    b = test_play2()
-    print(a/b)
-    print(b/a)
+    a = test_dodo(4)
+    print(a)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
-from hex_tools import *
 from typing import Callable
-import matplotlib.pyplot as plt
 import random
+import matplotlib.pyplot as plt
+from hex_tools import *
+
 
 Player = int
 R = 1
@@ -11,46 +12,28 @@ Action = tuple[Hex, Hex]
 Score = float
 
 
-class Box:
-    def __init__(self, q: int, r: int, p: Player):
-        self.coordinates = Hex(q, r)
-        self.player = p
-
-    def __str__(self):
-        return (
-            f"q: {self.coordinates.q}, r: {self.coordinates.r}, couleur: {self.player}"
-        )
-
-
 class Board:
-    def __init__(self, boxes: list[Box], n: int):
-        self.boxes = boxes
+    def __init__(self, grid: dict[Hex, Player], n: int):
+        self.grid = grid
         self.size = n
-        self.linkage_table = {box.coordinates.to_tuple(): box for box in boxes}
 
-    def legals(self, player: Player) -> list[tuple[Hex, Hex]]:
-        legals: list[tuple[Hex, Hex]] = []
-        for box in self.boxes:
-            if box.player == player == 1:
+    def legals(self, player: Player) -> list[Action]:
+        legals: list[Action] = []
+        for box in self.grid:
+            if self.grid[box] == player == 1:
                 for i in [1, 2, 3]:
                     if (
-                        box.coordinates.neighbor(i).to_tuple() in self.linkage_table
-                        and self.linkage_table[
-                            box.coordinates.neighbor(i).to_tuple()
-                        ].player
-                        == 0
+                        neighbor(box, i) in self.grid
+                        and self.grid[neighbor(box, i)] == 0
                     ):
-                        legals.append((box.coordinates, box.coordinates.neighbor(i)))
-            elif box.player == player == 2:
+                        legals.append((box, neighbor(box, i)))
+            elif self.grid[box] == player == 2:
                 for i in [0, 4, 5]:
                     if (
-                        box.coordinates.neighbor(i).to_tuple() in self.linkage_table
-                        and self.linkage_table[
-                            box.coordinates.neighbor(i).to_tuple()
-                        ].player
-                        == 0
+                        neighbor(box, i) in self.grid
+                        and self.grid[neighbor(box, i)] == 0
                     ):
-                        legals.append((box.coordinates, box.coordinates.neighbor(i)))
+                        legals.append((box, neighbor(box, i)))
         return legals
 
     def final(self, player: Player) -> bool:
@@ -58,13 +41,19 @@ class Board:
             return True
         return False
 
+    def play(self, player: Player, action: Action):
+        new_grid = self.grid.copy()
+        new_grid[action[0]] = 0
+        new_grid[action[1]] = player
+        return Board(new_grid, self.size)
+
     def pplot(self):
         plt.figure(figsize=(10, 10))
 
         layout = Layout(layout_flat, Point(1, -1), Point(0, 0))
-        for box in self.boxes:
-            corners = polygon_corners(layout, box.coordinates)
-            center = hex_to_pixel(layout, box.coordinates)
+        for box in self.grid:
+            corners = polygon_corners(layout, box)
+            center = hex_to_pixel(layout, box)
 
             # Contours de chaque hexagone
             list_edges_x = []
@@ -75,14 +64,14 @@ class Board:
             list_edges_x.append(list_edges_x[0])
             list_edges_y.append(list_edges_y[0])
             plt.plot(list_edges_x, list_edges_y, color="k")
-            if box.player == 1:
+            if self.grid[box] == 1:
                 plt.plot(center.x, center.y, marker="o", markersize=10, color="red")
-            elif box.player == 2:
+            elif self.grid[box] == 2:
                 plt.plot(center.x, center.y, marker="o", markersize=10, color="blue")
             plt.text(
                 center.x,
                 center.y,
-                f"{box.coordinates.q}, {box.coordinates.r}",
+                f"{box.q}, {box.r}",
                 horizontalalignment="right",
             )
         plt.xlim(-2 * self.size - 1, 2 * self.size + 1)
@@ -91,56 +80,23 @@ class Board:
 
 
 def start_board(size: int) -> Board:
-    list_boxes: list[Box] = []
+    grid: dict[Hex, Player] = {}
     n = size - 1
     for r in range(n, -n - 1, -1):
         q1 = max(-n, r - n)
         q2 = min(n, r + n)
         for q in range(q1, q2 + 1):
             if -q > r + (size - 3):
-                list_boxes.append(Box(q, r, R))
+                grid[Hex(q, r)] = R
             elif r > -q + (size - 3):
-                list_boxes.append(Box(q, r, B))
+                grid[Hex(q, r)] = B
             else:
-                list_boxes.append(Box(q, r, 0))
-    return Board(list_boxes, n)
+                grid[Hex(q, r)] = 0
+    return Board(grid, n)
 
 
 State = Board
 Strategy = Callable[[State, Player], Action]
-
-
-def play(b: Board, player: Player, action: tuple[Hex, Hex]) -> Board:
-    new_boxes: list[Box] = []
-    for box in b.boxes:
-        if not (
-            box.coordinates.q == action[0].q
-            and box.coordinates.r == action[0].r
-            and box.player == player
-        ) and not (
-            box.coordinates.q == action[1].q
-            and box.coordinates.r == action[1].r
-            and box.player == 0
-        ):
-            new_boxes.append(box)
-    new_boxes.append(Box(action[0].q, action[0].r, 0))
-    new_boxes.append(Box(action[1].q, action[1].r, player))
-    return Board(new_boxes, b.size)
-
-
-def play2(b: Board, player: Player, action: tuple[Hex, Hex]) -> Board:
-    # Create sets for the coordinates of the two actions
-    action_coords = [(action[0].q, action[0].r), (action[1].q, action[1].r)]
-
-    # Filter out boxes that match the conditions using set operations
-    new_boxes = [box for box in b.boxes if
-                 (box.coordinates.q, box.coordinates.r) not in action_coords]
-
-    # Add new boxes based on the action
-    new_boxes.append(Box(action[0].q, action[0].r, 0))
-    new_boxes.append(Box(action[1].q, action[1].r, player))
-
-    return Board(new_boxes, b.size)
 
 
 def strategy_brain(board: Board, player: Player) -> Action:
@@ -151,44 +107,34 @@ def strategy_brain(board: Board, player: Player) -> Action:
     end_r = int(input("end r :"))
     depart = Hex(start_q, start_r)
     arrive = Hex(end_q, end_r)
-    bool = False
-    for action in board.legals(player):
-        if action[0].q == depart.q and action[0].r == depart.r and action[1].q == arrive.q and action[1].r == arrive.r:
-            bool = True
 
-    while not bool:
+    while (depart, arrive) not in board.legals(player):
         print("Coup illégal !")
         start_q = int(input("start q :"))
         start_r = int(input("start r :"))
         end_q = int(input("end q :"))
         end_r = int(input("end r :"))
-        bool = False
-        for action in board.legals(player):
-            if action[0].q == depart.q and action[0].r == depart.r and action[1].q == arrive.q and action[1].r == arrive.r:
-                bool = True
+        depart = Hex(start_q, start_r)
+        arrive = Hex(end_q, end_r)
 
-    return Hex(start_q, start_r), Hex(end_q, end_r)
+    return depart, arrive
 
 
 def strategy_random(board: Board, player: Player) -> Action:
     return random.choice(board.legals(player))
 
 
-
-def dodo(
-    strategy_rouge: Strategy, strategy_bleu: Strategy, size: int, debug: bool = False
-) -> Score:
+def dodo(strategy_rouge: Strategy, strategy_bleu: Strategy, size: int) -> Score:
     b = start_board(size)
     b.pplot()
-    while not (b.final(1) or b.final(2)):
+    while True:
         s = strategy_rouge(b, 1)
-        b = play(b, 1, s)
-        b.pplot()
+        b = b.play(1, s)
         if b.final(2):
-            return -1
-        else:
-            s = strategy_bleu(b, 2)
-            b = play(b, 2, s)
             b.pplot()
-            if b.final(1):
-                return 1
+            return -1
+        s = strategy_bleu(b, 2)
+        b = b.play(2, s)
+        if b.final(1):
+            b.pplot()
+            return 1
