59f2538bb87d0511a1b31314f29a0f032ab8fbe7
Samuel.B <samuelbeziat64@gmail.com>
2024-05-20 14:09:22 +0200

    avancement

--- src/gameplay.py
+++ src/gameplay.py
@@ -127,8 +127,8 @@ def generic_strategy_dodo(
     if len(legals) == 1:
         return env, legals[0]
 
-    depth = env.adaptable_depth_v2(len(legals), len(legals_opp), 20000, 14)
-    # depth = 2
+    # depth = env.adaptable_depth_v2(len(legals), len(legals_opp), 20000, 14)
+    depth = 2
     list_moves: list[ActionDodo] = env.alphabeta_actions_v1(
         state,
         player,
--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ def main():
     # print(d.neighbors((-2, 1), R))
     # print(d.symetrical(d.grid))
     # d.pplot()
-    # match(4, 100, -5.52324828, 1.33774885, -1.21411309, -16.65091422, 8.74257623, -7.67015625)
+    match(6, 100, -5.52324828, 1.33774885, -1.21411309, -16.65091422, 8.74257623, -7.67015625)
     # match_vsrandom(4, 100, -16.65091422, 8.74257623, -7.67015625)
     # tuning_dodo(4, 10, 0.01)
     # tuning_dodo_v2(6, 10, 10000)
@@ -38,10 +38,10 @@ def main():
     # print(list_list)
     # for i in range(10000):
     #     new = tuple(dico.items())
-    d = EngineDodo(start_board_dodo(4), 4, 100)
-    # legal = ((-6, 0), (-5, 1))
-    for _ in range(200):
-        a = d.alphabeta_actions_v1(start_board_dodo(4), R, 5, float('-inf'), float('inf'), d.legals(R), 1, 0, 0)
+    # d = EngineDodo(start_board_dodo(4), 4, 100)
+    # # legal = ((-6, 0), (-5, 1))
+    # for _ in range(200):
+    #     a = d.alphabeta_actions_v1(start_board_dodo(4), R, 5, float('-inf'), float('inf'), d.legals(R), 1, 0, 0)
 
     # dodo_vsbrain(-3.22970786, 1.69112454, -1.18968059, 4, R)
     ##########################
