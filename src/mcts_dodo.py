826d176a3a761e57b32c152538ed1231a8d90f11
Samuel.B <samuelbeziat64@gmail.com>
2024-06-01 22:59:58 +0200

    test avec RAVE MCTS

--- src/mcts_dodo.py
+++ src/mcts_dodo.py
@@ -48,13 +48,17 @@ class GameState:
         self.CELLS: CellSet = cells
 
         # -------------------- Autres -------------------- #
-
-        self.legals: list[ActionDodo] = self.generate_legal_actions()
+        # hash_state = hash((tuple(self.grid.items()), self.player))
+        # if hash_state in legals_cache:
+        #     self.legals = legals_cache[hash_state]
+        # else:
+        self.legals: list[ActionDodo] = self.generate_legal_actions(self.player)
+            # legals_cache[hash_state] = self.legals
         self.move_stack: deque[tuple[ActionDodo, Player]] = deque()
 
-    def generate_legal_actions(self) -> list[ActionDodo]:
-        player_cells = self.R_CELLS if self.player == R else self.B_CELLS
-        neighbors = self.R_POV_NEIGHBORS if self.player == R else self.B_POV_NEIGHBORS
+    def generate_legal_actions(self, player: Player) -> list[ActionDodo]:
+        player_cells = self.R_CELLS if player == R else self.B_CELLS
+        neighbors = self.R_POV_NEIGHBORS if player == R else self.B_POV_NEIGHBORS
         grid = self.grid
 
         legals = [
@@ -89,40 +93,30 @@ class GameState:
             self.B_POV_NEIGHBORS,
         )
 
-    def _legals_for_simulation(self, player: Player) -> list[ActionDodo]:
-        player_cells = self.R_CELLS if player == R else self.B_CELLS
-        neighbors = self.R_POV_NEIGHBORS if player == R else self.B_POV_NEIGHBORS
-        grid = self.grid
-
-        legals = [
-            (cell, nghb)
-            for cell in player_cells
-            for nghb in neighbors[cell]
-            if grid[nghb] == 0
-        ]
-
-        return legals
-
-    def simulate_game(self) -> tuple[Player, int]:
+    def simulate_game(self) -> tuple[Player, int, set[ActionDodo], set[ActionDodo]]:
+        player_moves: set[ActionDodo] = set()
+        opponent_moves: set[ActionDodo] = set()
         while True:
-            legals: list[ActionDodo] = self._legals_for_simulation(self.player)
+            legals: list[ActionDodo] = self.generate_legal_actions(self.player)
             if len(legals) == 0:
                 winner = self.player
                 break
             move: ActionDodo = random.choice(legals)
+            player_moves.add(move)
             self.play(move, self.player)
 
-            legals = self._legals_for_simulation(self.opponent)
+            legals = self.generate_legal_actions(self.opponent)
             if len(legals) == 0:
                 winner = self.opponent
                 break
             move = random.choice(legals)
+            opponent_moves.add(move)
             self.play(move, self.opponent)
 
         game_length: int = len(self.move_stack)
         self.undo_stack()
 
-        return winner, game_length
+        return winner, game_length, player_moves, opponent_moves
 
     def play(self, action, player) -> None:
         self.move_stack.append((action, player))
@@ -146,6 +140,9 @@ class GameState:
             else:
                 self.B_CELLS.remove(action[1])
                 self.B_CELLS.add(action[0])
+
+    def evaluate(self):
+        return 1/(1+len(self.generate_legal_actions(self.opponent))) - 1/(1+len(self.get_legal_actions()))
 
     def pplot(self) -> None:
         """
@@ -198,6 +195,8 @@ class MonteCarloTreeSearchNode:
         state: GameState,
         player: Player,
         c: float,
+        rave_const: float,
+        p,
         parent=None,
         parent_action: Optional[ActionDodo] = None,
     ):
@@ -207,10 +206,14 @@ class MonteCarloTreeSearchNode:
         self.children: list[MonteCarloTreeSearchNode] = []
         self.Q: int = 0
         self.N: int = 0
+        self.Q_AMAF: int = 0
+        self.N_AMAF: int = 0
         self.untried_actions: list[ActionDodo] = self.initialize_actions()
         self.player: Player = player
         self.opponent: Player = R if self.player == B else B
         self.c = c
+        self.rave_const = rave_const
+        self.p = p
 
     def initialize_actions(self) -> list[ActionDodo]:
         self.untried_actions = self.state.get_legal_actions().copy()
@@ -220,7 +223,7 @@ class MonteCarloTreeSearchNode:
         action: ActionDodo = self.untried_actions.pop()
         next_state: GameState = self.state.move(action)
         child_node: MonteCarloTreeSearchNode = MonteCarloTreeSearchNode(
-            next_state, self.player, self.c, parent=self, parent_action=action
+            next_state, self.player, self.c, self.rave_const, self.p, parent=self, parent_action=action
         )
 
         self.children.append(child_node)
@@ -229,13 +232,13 @@ class MonteCarloTreeSearchNode:
     def is_terminal_node(self) -> bool:
         return self.state.is_game_over()
 
-    def rollout(self) -> tuple[int, int]:
+    def rollout(self) -> tuple[int, int, set[ActionDodo], set[ActionDodo]]:
         current_rollout_state: GameState = self.state
-        result, game_length = current_rollout_state.simulate_game()
+        result, game_length, current_player_moves, current_opponent_moves = current_rollout_state.simulate_game()
 
-        if result != self.state.player:
-            return 1, game_length
-        return 0, game_length
+        if result != self.state.player:  # Reward for the player who has played, not the one who must play
+            return 1, game_length, current_opponent_moves, current_player_moves
+        return 0, game_length, current_player_moves, current_opponent_moves
 
     def backpropagate(self, result) -> None:
         self.N += 1
@@ -243,15 +246,31 @@ class MonteCarloTreeSearchNode:
         if self.parent:
             self.parent.backpropagate(1 - result)
 
+    def amaf_backup(self, winning_moves, losing_moves):
+        if self.parent:
+            for child in self.parent.children:
+                if child.parent_action in winning_moves:
+                    child.N_AMAF += 1
+                    child.Q_AMAF += 1
+                if child.parent_action in losing_moves:
+                    child.N_AMAF += 1
+            self.parent.amaf_backup(losing_moves, winning_moves)
+
     def is_fully_expanded(self) -> bool:
         return len(self.untried_actions) == 0
 
     def best_child(self, c_param=0.1) -> MonteCarloTreeSearchNode:
-        choices_weights: list[float] = [
+        alpha = 0 if self.rave_const == 0 else max(0., (self.rave_const - self.N) / self.rave_const)
+
+        UCT_choice = np.array([
             (c.Q / c.N) + c_param * np.sqrt((2 * np.log(self.N) / c.N))
             for c in self.children
-        ]
-        return self.children[np.argmax(choices_weights)]
+        ])
+        AMAF_choice = np.array([
+            c.Q_AMAF / c.N_AMAF if c.N_AMAF != 0 else 0
+            for c in self.children
+        ])
+        return self.children[np.argmax((1-alpha)*UCT_choice + alpha*AMAF_choice)]
 
     def best_final_child(self) -> MonteCarloTreeSearchNode:
         choices_weight: list[int] = [c.N for c in self.children]
@@ -273,31 +292,58 @@ class MonteCarloTreeSearchNode:
 
     def best_action(
         self, allocated_time: float
-    ) -> tuple[MonteCarloTreeSearchNode, int]:
+    ) -> tuple[MonteCarloTreeSearchNode, int|None]:
         length_count: int = 0
         simulation_count: int = 1
         start_time: float = time.time()
+        if self.is_fully_expanded() and len(self.children) == 1:
+            return self.best_final_child(), None
         while time.time() - start_time < allocated_time:
             v: MonteCarloTreeSearchNode = self._tree_policy()
-            reward, game_length = v.rollout()
+            reward, game_length, winning_moves, losing_moves = v.rollout()
             length_count += game_length
             simulation_count += 1
             v.backpropagate(reward)
+            v.amaf_backup(winning_moves, losing_moves)
+
+            if simulation_count % 50 == 0:
+                first_visited, second_visited = self.get_two_most_visited()
+                time_spent = time.time() - start_time
+                time_left = allocated_time - time_spent
+                if simulation_count * (time_left/time_spent) * self.p < first_visited - second_visited:
+                    break
         print(simulation_count)
         return self.best_final_child(), max(int(length_count / simulation_count), 2)
+
+    def get_two_most_visited(self):
+        if not self.children or len(self.children) < 2:
+            raise ValueError("The node should have at least two children.")
+
+        first_max = second_max = float('-inf')
+
+        for child in self.children:
+            if child.N > first_max:
+                second_max = first_max
+                first_max = child.N
+            elif child.N > second_max:
+                second_max = child.N
+
+        return first_max, second_max
 
     def __str__(self):
         return (
             f"Parent action : {self.parent_action}\n"
             f"|-- Number simulations : {self.N}\n"
             f"|-- Number of victories : {self.Q}\n"
+            f"|-- AMAF count : {self.N_AMAF}\n"
+            f"|-- AMAF wins : {self.Q_AMAF}\n"
             f"|-- Ratio : {self.Q/self.N:.3f}"
         )
 
 
 class Engine:
     def __init__(
-        self, state: Grid, player: Player, hex_size: int, total_time: Time, c: float
+        self, state: Grid, player: Player, hex_size: int, total_time: Time, c: float, rave_const: float, p, f
     ):
 
         # -------------------- Attributs généraux -------------------- #
@@ -315,6 +361,9 @@ class Engine:
         # -------------------- Monte Carlo Tree Searcher -------------------- #
 
         self.c: float = c
+        self.rave_const = rave_const
+        self.p = p
+        self.f = f
         self.MCTSearcher: MonteCarloTreeSearchNode = MonteCarloTreeSearchNode(
             GameState(
                 state,
@@ -326,6 +375,8 @@ class Engine:
             ),
             player,
             c,
+            rave_const,
+            p
         )
 
         # -------------------- Time Allocator -------------------- #
@@ -354,14 +405,15 @@ class Engine:
         }
 
     def select_best_move(self, time_left: float) -> ActionDodo:
-        time_allocated: float = self.time_manager.simple_time_allocation(
+        time_allocated: float = self.f * self.time_manager.simple_time_allocation(
             time_left, self.previous_mean_game_length
         )
         best_children, mean_game_length = self.MCTSearcher.best_action(time_allocated)
         self.MCTSearcher = best_children
         self.MCTSearcher.parent = None
-        self.previous_mean_game_length = mean_game_length
-        print(f"{time_left:.2f}, {time_allocated:.2f}, {mean_game_length:.2f}")
+        if mean_game_length is not None:
+            self.previous_mean_game_length = mean_game_length
+            # print(f"{time_left:.2f}, {time_allocated:.2f}, {mean_game_length:.2f}")
         print(best_children)
         return best_children.parent_action
 
@@ -378,6 +430,8 @@ class Engine:
                 next_state,
                 self.player,
                 self.c,
+                self.rave_const,
+                self.p,
                 parent=self.MCTSearcher,
                 parent_action=has_played,
             )
@@ -423,11 +477,11 @@ def state_to_dict(state: State, size: int):
 
 
 def initialize(
-    game: str, state: State, player: Player, hex_size: int, total_time: Time, c: float
+    game: str, state: State, player: Player, hex_size: int, total_time: Time, c: float, rave_const: float, p, f
 ) -> Environment:
     if game.lower() == "dodo":
         grid = state_to_dict(state, hex_size)
-        return Engine(grid, player, hex_size, total_time, c)
+        return Engine(grid, player, hex_size, total_time, c, rave_const, p, f)
     else:
         pass
 
@@ -451,27 +505,29 @@ def new_state_dodo(state: State, action: Action, player: Player):
     state.append((action[1], player))
 
 
-def dodo(size: int, c1, c2):
+def dodo(size: int, c1, r1, p1, f1, c2, r2, p2, f2):
     state_tmp = start_board_dodo(size)
-    e1 = initialize("dodo", state_tmp, R, size, 120, c1)
-    e2 = initialize("dodo", state_tmp, B, size, 120, c2)
-    time_r: float = 120.0
-    time_b: float = 120.0
+    e1 = initialize("dodo", state_tmp, R, size, 120, c1, r1, p1, f1)
+    e2 = initialize("dodo", state_tmp, B, size, 120, c2, r2, p2, f2)
+    time_r: float = 120.0/2
+    time_b: float = 120.0/2
     i = 0
     while True:
         start_time = time.time()
         s = strategy(e1, state_tmp, e1.player, time_r)
         if s is None:
+            e1.MCTSearcher.state.pplot()
             print(1, end="")
             return 1
         time_r -= time.time() - start_time
-        if i % 20 == 0:
-            e1.MCTSearcher.state.pplot()
+        # if i % 5 == 0:
+        #     e1.MCTSearcher.state.pplot()
         new_state_dodo(state_tmp, s, R)
 
         start_time = time.time()
         s = strategy(e2, state_tmp, e2.player, time_b)
         if s is None:
+            e2.MCTSearcher.state.pplot()
             print(2, end="")
             return -1
         time_b -= time.time() - start_time
@@ -480,16 +536,16 @@ def dodo(size: int, c1, c2):
 
 
 def wrapper(args):
-    c1, c2, size = args
-    return dodo(size, c1, c2)
+    c1, r1, p1, f1, c2, r2, p2, f2, size = args
+    return dodo(size, c1, r1, p1, f1, c2, r2, p2, f2)
 
 
-def match(nb_games: int, size: int, c1, c2):
+def match(nb_games: int, size: int, c1, r1, p1, f1, c2, r2, p2, f2):
     nb_wins = 0
     nb_losses = 0
 
     with multiprocessing.Pool() as pool:
-        args_list = [(c1, c2, size)] * (nb_games // 2)
+        args_list = [(c1, r1, p1, f1, c2, r2, p2, f2, size)] * (nb_games // 2)
         results = pool.map(wrapper, args_list)
 
     for result in results:
@@ -499,7 +555,7 @@ def match(nb_games: int, size: int, c1, c2):
             nb_losses += 1
 
     with multiprocessing.Pool() as pool:
-        args_list = [(c2, c1, size)] * (nb_games // 2)
+        args_list = [(c2, r2, p2, f2, c1, r1, p1, f1, size)] * (nb_games // 2)
         results = pool.map(wrapper, args_list)
     for result in results:
         if result == -1:
@@ -507,7 +563,7 @@ def match(nb_games: int, size: int, c1, c2):
         else:
             nb_losses += 1
 
-    print(f"\n({c1:.3f}) vs ({c2:.3f})")
+    print(f"\n({c1:.3f}, {r1}, {p1}, {f1}) vs ({c2:.3f}, {r2}, {p2}, {f2})")
     print_percentage_bar(nb_wins / nb_games, nb_losses / nb_games, nb_games)
     return nb_wins, nb_losses
 
@@ -549,7 +605,7 @@ def main():
     # for _ in range(100):
     #     e = initialize("dodo", start_board_dodo(4), R, 4, 100, 1.)
     #     a = e.select_best_move()
-    dodo(8, 0.5, 0.1)
+    dodo(7, 1, 0, 0.2, 1.5, 1, 0, 0.8, 5)
     # print(strategy(e, start_board_dodo(4), R, 100))
     # tab = []
     # for _ in range(1):
@@ -560,11 +616,11 @@ def main():
     #
     # print(tab.count(1), tab.count(-1))
     # v = []
-    # for c in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
-    #     v.append(match(100, 4, c, 0)[0])
+    # for r in [10, 20, 30, 40]:
+    #     v.append(match(100, 4, 1, r, 1, 0)[0])
     # plt.plot(v)
     # plt.show()
-    # match(10, 4, 0.1, 1)
+    # match(100, 4, 1, 0, 1, 50)
     # tuning_dodo(4, 10)
 
 
