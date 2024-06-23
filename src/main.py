826fbaa9322b1827b62db8e63f9fd8ef4ab833b5
SamuelB348 <133508956+SamuelB348@users.noreply.github.com>
2024-06-23 21:43:46 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     parser.add_argument("group_id")
     parser.add_argument("members")
     parser.add_argument("password")
-    parser.add_argument("-s", "--server-url", default="http://lchappuis.fr:8080/")
+    parser.add_argument("-s", "--server-url", default="http://localhost:8080/")
     parser.add_argument("-d", "--disable-dodo", action="store_true")
     parser.add_argument("-g", "--disable-gopher", action="store_true")
     args = parser.parse_args()
